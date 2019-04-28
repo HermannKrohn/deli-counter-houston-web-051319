96fb5c3b1a04bf20b2f54893a195be413d9350d6
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-04-28 18:15:24 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+def line(array)
+  if array.size == 0 
+    puts "The line is currently empty."
+  else
+    printf("The line is currently: ")
+    array.each_with_index{|item, index|
+    printf("#{index + 1}. #{item}")
+  end
+end
+
+  
+  
